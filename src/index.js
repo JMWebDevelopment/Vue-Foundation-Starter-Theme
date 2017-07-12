908867f436f9fa9fbada1bdd05047bb4a7865e5a
ViewFromTheBox <jacob.martella@att.net>
2017-07-12 09:38:35 -0500

    Started building the Vue App

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Vuex from 'vuex';
-import { HTTP as axios } from './assets/axios/axios';
-import VueProgressiveImage from 'progressive-image/dist/vue';
 
-Vue.prototype.$http = axios;
+Vue.config.productionTip = false
 
-Vue.use( Vuex );
-Vue.use( VueRouter );
-Vue.use( VueProgressiveImage, {
-    removePreview: true
-} );
-
-Vue.config.debug = true;
-Vue.config.devTools = true;
+/* eslint-disable no-new */
+/*new Vue({
+    el: '#app',
+    router,
+    template: '<App/>',
+    components: { App }
+})*/
