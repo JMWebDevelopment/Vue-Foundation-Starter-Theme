e23b5a376f21a1ea6c27cb45227898c7692091b0
Jacob Martella <jacob.martella@att.net>
2017-07-12 19:04:15 -0500

    Fixed page title

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import Vue from 'vue';
 import router from './router'
 import App from './app.vue'
+import Vuex from 'vuex';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use( Vuex );
+
+//Define vuex store
+const store = new Vuex.Store( {
+    state: {
+        title: ''
+    },
+    mutations: {
+        rtChangeTitle( state, value ) {
+            // mutate state
+            state.title = value;
+            document.title = ( state.title ? state.title + ' - ' : '' ) + rtwp.site_name;
+        }
+    }
+} );
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: { App }
 })
