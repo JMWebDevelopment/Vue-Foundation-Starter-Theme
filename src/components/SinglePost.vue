5940f009f58641969bb75c49b31fd09aaf545a5d
ViewFromTheBox <jacob.martella@att.net>
2017-07-12 22:14:02 -0500

    Finished setting up single post template

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,61 +1,81 @@
 <template>
-    <div class="post">
-        <section class="hero">
-            <div class="hero-body row">
-                <div class="container large-12 columns">
-                    <h1 class="title is-1" v-html="post.title.rendered"></h1>
-                    <h2 class="subtitle" v-if="author">Written by <router-link :to="{ name:'Author', params: {authorId: author.id} }">{{ author.name }}</router-link></h2>
-                </div>
-            </div>
-        </section>
+		<transition name="slide-fade">
 
-        <div class="content-area row">
-            <section class="content-container large-12 columns" v-html="post.content.rendered"></section>
-        </div>
-    </div>
+			<div class="row main" v-if="loaded === 'true'">
+
+				<div class="large-12 medium-12 small-12 column" >
+
+					<div class="post">
+
+						<h2 class="post-title"> {{ post.title.rendered }}</h2>
+
+						<h3 class="subtitle" v-if="author">
+                                                Written by <router-link :to="{ name:'Author', params: {authorId: author.id} }">{{ author.name }}</router-link>
+                                            </h3>
+
+						<div class="progressive full">
+
+							<img class="lazy"
+								v-progressive="post.featured_image_src['full'][0]"
+								:data-srcset="post.featured_image_src['srcset']"
+								:src="post.featured_image_src['full'][0]"
+							/>
+						</div>
+
+						<div class="post-content rt-content" v-html="post.content.rendered" ></div>
+
+						<div class="cat-list">
+							<router-link v-for="x in post.cat_name" :to="{ name: 'cat', params: { name:x.slug } }" :key="x.id"> {{ x.name }} </router-link>
+						</div>
+
+						<div class="tag-list">
+							<router-link v-for="x in post.tag_name" :to="{ name: 'tag', params: { name:x.slug } }" :key="x.id"> {{ x.name }} </router-link>
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</transition>
+
 </template>
 
 <script>
 import axios from 'axios'
 
-export default{
-  name: 'Single',
-  data () {
-    return {
-      title: '',
-      post: [],
-      author: []
-    }
-  },
-  created () {
-    this.getPost()
-  },
-  methods: {
-    getPost () {
-      const vm = this;
-      var path = this.$route.path
-      axios.get(vuefoundationstarter.root + 'wp/v2/posts/?slug=' + this.$route.params.postSlug, '')
-      .then(response => {
-        this.post = response.data[0]
-        if (this.post === undefined) {
-          //this.$router.push({name: 'NotFound'})
-        }
-        // Get author
-        this.getAuthor()
-        // Set document title
-        //document.title = this.post.title.rendered
-        //vm.$store.commit( 'themeSlugChangeTitle', document.title );
-      })
-      .catch(e => {
-        console.log(e)
-      })
-    },
-    getAuthor () {
-      axios.get(vuefoundationstarter.root + 'wp/v2/users/' + this.post.author)
-      .then(response => {
-        this.author = response.data
-      })
-    }
-  }
-}
+export default {
+	mounted: function() {
+		this.getPost();
+	},
+	data() {
+		return {
+			base_path: vuefoundationstarter.base_path,
+			post: {},
+			author: {},
+			loaded: 'false',
+			pageTitle: ''
+		};
+	},
+	methods: {
+		getPost: function() {
+			const vm = this;
+			axios.get( vuefoundationstarter.root + 'wp/v2/posts/?slug=' + vm.$route.params.postSlug )
+			.then( ( res ) => {
+				vm.post = res.data[ 0 ];
+				vm.loaded = 'true';
+				vm.pageTitle = vm.post.title.rendered;
+				vm.$store.commit( 'themeSlugChangeTitle', vm.pageTitle );
+
+				axios.get(vuefoundationstarter.root + 'wp/v2/users/' + this.post.author)
+                      .then(response => {
+                        this.author = response.data
+                      })
+			} )
+			.catch( ( res ) => {
+				//console.log( `Something went wrong : ${res}` );
+			} );
+		}
+	}
+};
 </script>
