6cda14981d2c009abc465574ba9d32b8d07a43a9
ViewFromTheBox <jacob.martella@att.net>
2017-07-16 19:16:43 -0500

    Added Pagination to Day Template

--- src/components/Day.vue
+++ src/components/Day.vue
@@ -27,7 +27,7 @@
 
                         <div class="progressive full" v-if="post.featured_image_src['full'][0]">
 
-                            <img class="lazy" v-progressive="post.featured_image_src['full'][0]" :data-srcset="post.featured_image_src['srcset']" :src="post.featured_image_src['full'][0]" />
+                            <img class="lazy" :src="post.featured_image_src['full'][0]" />
 
                         </div>
 
@@ -35,6 +35,15 @@
 
                     </div>
 
+                </div>
+
+                <div class="pagination row">
+                    <div v-if="page > 1" class="newer-posts large-6 medium-6 small-6 columns">
+                        <router-link :to="{ name: 'Day', query: { page: this.page - 1 }} " class="pagination-previous button" v-on:click.native="previousClick">Previous</router-link>
+                    </div>
+                    <div v-if="page < totalPages" class="newer-posts large-6 medium-6 small-6 columns">
+                        <router-link :to="{ name: 'Day', query: { page: this.page + 1 }}" class="pagination-next button" v-on:click.native="nextClick">Next</router-link>
+                    </div>
                 </div>
 
             </div>
@@ -59,13 +68,19 @@
                 "August", "September", "October",
                 "November", "December" ];
 
+            if ( this.$route.query.page ) {
+            } else {
+                this.$route.query.page = 1;
+            }
             return {
-                page: {},
+                posts: {},
                 day: this.$route.params.day,
                 month: monthNames[ this.$route.params.month - 1 ],
                 year: this.$route.params.year,
                 loaded: 'false',
-                pageTitle: ''
+                pageTitle: '',
+                page: this.$route.query.page,
+                totalPages: ''
             };
         },
         methods: {
@@ -84,9 +99,15 @@
 
                     var endDate = new Date(vm.$route.params.year, vm.$route.params.month + 1, this.$route.params.day + 1, 0, 0, 0, 0);
 
-                    axios.get( vuefoundationstarter.root + 'wp/v2/posts?after=' + startDate.toISOString() + '&before=' + endDate.toISOString() )
+                    if (this.$route.query.page !== undefined) {
+                        vm.postsUrl = '&page=' + vm.$route.query.page;
+                    } else {
+                        vm.postsUrl = '';
+                    }
+                    axios.get( vuefoundationstarter.root + 'wp/v2/posts?after=' + startDate.toISOString() + '&before=' + endDate.toISOString() + this.postsUrl )
                         .then( ( res ) => {
                             vm.posts = res.data;
+                            vm.totalPages = res.headers['x-wp-totalpages'];
                             vm.loaded = 'true';
                             const monthNames = [ "January", "February", "March",
                                 "April", "May", "June", "July",
@@ -119,7 +140,17 @@
                     const year = date.getFullYear();
                     return monthNames[ monthIndex ] + ',' + day + ' ' + year;
                 }
-            }
+            },
+            previousClick () {
+                this.page = this.page - 1;
+                this.getCatId(this.$route.params.categorySlug);
+                window.scrollTo(0, 0);
+            },
+            nextClick () {
+                this.page = this.page + 1;
+                this.getCatId(this.$route.params.categorySlug);
+                window.scrollTo(0, 0);
+            },
         },
         watch: {
             '$route'( to, from ) {
